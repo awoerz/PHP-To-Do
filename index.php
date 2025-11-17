57b52f48ea1497bd4b7fe5d886d297ec423336c1
Adam <adamibc@yahoo.com>
2025-11-16 22:25:48 -0500

    Added a header a footer partial file for future use.

--- index.php
+++ index.php
@@ -1,67 +1,38 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>PHP To-Do</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+<?php include('./shared/header.php'); ?>
 
-        <style>
-            .spacer {
-                margin-bottom: .5rem;
-            }
-            .add-item-form {
-                max-width: 20rem;
-                display: flex;
-                flex-direction: column;
-            }
-            .list-item {
-                display: flex;
-                margin-bottom: .5rem;
-            }
-            .list-item-view {
-                margin: auto 0;
-            }
-            .remove-item-form {
-                margin-right: 1rem;
-            }
-        </style>
-</head>
-<body>
-    
-    <?php
-        require_once __DIR__ . '/config/config.php';
-        $datafile =  __DIR__ . '/storage/data.json';
-        $list = json_decode(file_get_contents($datafile), true);
-    ?>
+<?php
+    require_once __DIR__ . '/config/config.php';
+    $datafile =  __DIR__ . '/storage/data.json';
+    $list = json_decode(file_get_contents($datafile), true);
+?>
 
-    <div class="container">
-        <h1>PHP To-Do</h1>
+<div class="container">
+    <h3>Add Item</h3>
 
-        <h3>Add Item</h3>
+    <form class="add-item-form" action="action.php" method="post">
+        <label class="spacer" for="item">New item</label>
+        <input class="spacer" name="item" type="text">
+        <input type="hidden" name="action" value="add">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
-        <form class="add-item-form" action="action.php" method="post">
-            <label class="spacer" for="item">New item</label>
-            <input class="spacer" name="item" type="text">
-            <input type="hidden" name="action" value="add">
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+    <br>
 
-        <br>
-    
-        <h3>Items</h3>
-        <?php if(empty($list)) :  ?>
-            <p class="text-muted">No items yet.</p>
-        <?php else : ?>
-            <?php foreach ($list as $item): ?>
-                <div class=list-item>
-                    <form class="remove-item-form" action="action.php" method="post">
-                        <input type="hidden" name="action" value="remove">
-                        <input type="hidden" name="id" value="<?php echo $item['id'] ?>">
-                        <button type="submit" class="btn btn-danger btn-sm">-</button>
-                    </form>
-                    <p class="list-item-view"><?php echo htmlspecialchars($item['value'])?></p>
-                </div>
-            <?php endforeach; ?>
-         <?php endif; ?>
-    </div>
-</body>
+    <h3>Items</h3>
+    <?php if(empty($list)) :  ?>
+        <p class="text-muted">No items yet.</p>
+    <?php else : ?>
+        <?php foreach ($list as $item): ?>
+            <div class=list-item>
+                <form class="remove-item-form" action="action.php" method="post">
+                    <input type="hidden" name="action" value="remove">
+                    <input type="hidden" name="id" value="<?php echo $item['id'] ?>">
+                    <button type="submit" class="btn btn-danger btn-sm">-</button>
+                </form>
+                <p class="list-item-view"><?php echo htmlspecialchars($item['value'])?></p>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
+
+<?php include('./shared/footer.php'); ?>
---
+++ shared/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP To-Do</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+        <style>
+            .spacer {
+                margin-bottom: .5rem;
+            }
+            .add-item-form {
+                max-width: 20rem;
+                display: flex;
+                flex-direction: column;
+            }
+            .list-item {
+                display: flex;
+                margin-bottom: .5rem;
+            }
+            .list-item-view {
+                margin: auto 0;
+            }
+            .remove-item-form {
+                margin-right: 1rem;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar text-bg-primary mb-3">
+            <div class="container text-bg-primary">
+                <span class="navbar-brand mb-0 h1 text-bg-primary">PHP To Do App</span>
+            </div>
+        </nav>
